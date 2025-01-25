377579f9b872e5911d50fe2e5beedacd524ba034
Jordan Glass <jordanglass21@gmail.com>
2025-01-25 12:53:31 -0500

    gallery_props update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,12 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+// import Profile from './components/profile'
+import Gallery from './qcomps/gallery_props'
 
 export default function Home() {
   return (
-    <Profile />
+    <Gallery />
     // test
   )
 }
--- src/pages/qcomps/gallery_props.tsx
+++ src/pages/qcomps/gallery_props.tsx
@@ -1,3 +1,70 @@
+// my version
+
+import { GalleryProps } from "@/types/gallery";
+
+/**
+ * Displays a scientist's profile with an image and details.
+ * @param {object} props - The component props.
+ * @param {object} props.scientist - The scientist's details.
+ * @param {string} props.scientist.name - The name of the scientist.
+ * @param {string} props.scientist.imageId - The image ID for the scientist's photo.
+ * @param {string} props.scientist.profession - The scientist's profession.
+ * @param {string} props.scientist.awards - The scientist's awards.
+ * @param {string} props.scientist.discovery - The key discovery of the scientist.
+ * @param {number} [props.size=100] - The image size in pixels (default is 100).
+ * @returns {JSX.Element} A profile section with the scientist's information.
+ */
+function Scientist({scientist, size = 100}: GalleryProps) {
+  return (
+    <section className="profile">
+        <h2>{scientist.name}</h2>
+        <img
+          className="avatar"
+          src={'https://i.imgur.com/' + scientist.imageId + '.jpg'}
+          alt={scientist.name}
+          width={size}
+          height={size}
+        />
+        <ul>
+          <li>
+            <b>Profession: </b>
+            {scientist.profession}
+          </li>
+          <li>
+            <b>Awards: 4 </b>
+            {scientist.awards}
+          </li>
+          <li>
+            <b>Discovered: </b>
+            {scientist.discovery}
+          </li>
+        </ul>
+      </section>
+  )
+}
+
+/**
+ * Displays a gallery of notable scientists.
+ * @returns {JSX.Element} A list of Scientist components.
+ */
+export default function Gallery() {
+  return (
+    <div>
+      <h1>Notable Scientists</h1>
+      <Scientist
+        scientist={{name: 'Maria Skłodowska-Curie', imageId: 'szV5sdGs', profession: 'physicist and chemist',
+          awards: '(Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)', discovery: 'polonium (element)'}}
+        size={70} />
+      <Scientist
+        scientist={{name: 'Katsuko Saruhashi', imageId: 'YfeOqp2s', profession: 'geochemist',
+          awards: '(Miyake Prize for geochemistry, Tanaka Prize)', discovery: 'a method for measuring carbon dioxide in seawater'}}
+        size={70} />
+    </div>
+  );
+}
+
+// original version
+/*
 export default function Gallery() {
   return (
     <div>
@@ -53,3 +120,4 @@ export default function Gallery() {
     </div>
   );
 }
+*/
